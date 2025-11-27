169759fbb064ba83d0c1454908854a4f0beda19d
MOYE <56973455+lasertsole@users.noreply.github.com>
2025-11-27 10:30:57 +0800

    Update index.vue

--- front-end/pages/index.vue
+++ front-end/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-   <div class="indexPage">
-        <div class="top">
-            <div class="info">
-               <div class="title">定制需求，成品交易，一举两得</div>
-           </div>
+<div class="indexPage">
+     <div class="top">
+         <div class="info">
+            <div class="title">定制需求，成品交易，一举两得</div>
         </div>
-        <div class="bottom">
-        </div>
-   </div>
+     </div>
+     <div class="bottom">
+     </div>
+</div>
 </template>
 
 <script lang="ts" setup>
@@ -18,41 +18,41 @@ definePageMeta({
 </script>
 
 <style lang="scss" scoped>
-    @use "sass:math";
-    @use "@/common.scss" as common;
+ @use "sass:math";
+ @use "@/common.scss" as common;
 
-    .indexPage{
-       @include common.fullInParent;
-       position: relative;
-       z-index: 1;
-       .top{
-           @include common.fullInParent;
-           position: absolute;
-           z-index: 1;
-           background-color: rgba($color: #000000, $alpha: .2);
-           display: flex;
-           flex-direction: column;
-           justify-content: center;
-           align-items: center;
-           .info{
-               max-width: 700px;
-               height: 370px;
-               display: flex;
-               flex-direction: column;
-               justify-content: center;
-               align-items: center;
-               .title{
-                   font-size: 34px;
-                   color: white;
-                   font-weight: bold;
-                   padding: 0px 20px;
-               }
-           }
-       }
-        .bottom{
-           @include common.fullInParent;
-           position: relative;
-           z-index: 0;
+ .indexPage{
+    @include common.fullInParent;
+    position: relative;
+    z-index: 1;
+    .top{
+        @include common.fullInParent;
+        position: absolute;
+        z-index: 1;
+        background-color: rgba($color: #000000, $alpha: .2);
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .info{
+            max-width: 700px;
+            height: 370px;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            .title{
+                font-size: 34px;
+                color: white;
+                font-weight: bold;
+                padding: 0px 20px;
+            }
         }
-   }
+    }
+     .bottom{
+        @include common.fullInParent;
+        position: relative;
+        z-index: 0;
+     }
+}
 </style>
